202a8c0b479a59b8bef638bccd4c5e990bdac121
VenancioIgrejas <venancio.igrejas@poli.ufrj.br>
2018-11-01 23:05:16 -0300

    add some funtions for ensemble module and MLP

--- Functions/util.py
+++ Functions/util.py
@@ -10,13 +10,23 @@ def timer(name):
     print('[{0}] done in {1}'.format(name,time.time() - t0))
 
 
-def check_mount_dict(dict_defaul,dict_pass):
-    if dict_pass is None:
+def check_mount_dict(dict_defaul,list_of_tubles):
+
+    if list_of_tubles is None:
         return dict_defaul
-    else:
-        for keys,values in dict_pass.items():
-            dict_defaul[keys] = dict_pass[keys]
+
+    dict_pass = dict(list_of_tubles)
+
+    for keys,values in dict_pass.items():
+        dict_defaul[keys] = dict_pass[keys]
         return dict_defaul
+
+def update_paramns(old_dict,new_dict,exception=[]):
+    for keys in old_dict:
+        if not keys in exception:
+            old_dict[keys] = new_dict[keys]
+    return old_dict
+
 
 def get_objects(object,name,kwargs):
     obj = getattr(object,name)
