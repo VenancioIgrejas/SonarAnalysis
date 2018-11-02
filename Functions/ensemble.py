9d0fb8201b38f124907c4e600f6417a96d3cfdbb
VenancioIgrejas <venancio.igrejas@poli.ufrj.br>
2018-11-01 23:03:59 -0300

    add the possibility of save model when use AdaBoost

--- Functions/ensemble.py
+++ Functions/ensemble.py
@@ -1,8 +1,10 @@
 import numpy as np
+import os
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.base import BaseEstimator,ClassifierMixin
+from sklearn.ensemble.base import BaseEnsemble,_set_random_states
+from sklearn.base import BaseEstimator,ClassifierMixin,clone
 
 
 class AdaBoost(AdaBoostClassifier):
@@ -19,6 +21,30 @@ class AdaBoost(AdaBoostClassifier):
                                        algorithm=algorithm,
                                        random_state=random_state)
         self.le_=None
+
+
+
+    def _make_estimator(self, append=True, random_state=None):
+        """Make and configure a copy of the `base_estimator_` attribute.
+        Warning: This method should be used to properly instantiate new
+        sub-estimators.
+        """
+        estimator = clone(self.base_estimator_)
+
+        path = getattr(estimator,'dir') + '{0}_estimator'.format(len(self.estimators_))
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        estimator.set_params(**dict([('dir',path)]))
+
+        if random_state is not None:
+            _set_random_states(estimator, random_state)
+
+        if append:
+            self.estimators_.append(estimator)
+
+        return estimator
 
     def set_classes(self,y):
         """ Enconde label"""
