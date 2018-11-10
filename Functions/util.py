8bbdd29e4f7f45d88ceaa994f1540beb0ee4a739
VenancioIgrejas <venancio.igrejas@poli.ufrj.br>
2018-11-10 20:06:50 -0200

    add invert_dict function

--- Functions/util.py
+++ Functions/util.py
@@ -40,3 +40,43 @@ def file_exist(path_file):
     """check if a file exists. If exists, return True
     """
     return os.path.isfile(path_file)
+
+def best_file(path_files,choose_key,path_rename_file=None):
+    """ return the choose file in a dictionary of files
+    ----------
+    path_files : {dict}
+               Dictionary of path files
+
+    best_key : {key of dictionary}
+              key of the choose file
+    path_rename_file : {string}
+                     rename the file choosed
+    Returns
+    ----------
+    the chosen files' path according to choose_key
+    """
+
+
+    if not path_rename_file is None:
+        os.rename(path_files[choose_key], path_rename_file)
+        path_files[choose_key] = path_rename_file
+
+    for keys in path_files:
+        if not keys is choose_key:
+            if os.path.exists(path_files[keys]):
+                os.remove(path_files[keys])
+
+    return path_files[choose_key]
+
+def invert_dict(dicto):
+    """ invert key,value in dictionary [{key:value} -> {value:key}]
+    """
+    tmp = []
+    for key,values in dicto.items():
+        if isinstance(values,(list,np.ndarray)):
+            for subvalues in values:
+                tmp.append((subvalues,key))
+        else:
+            tmp.append((values,key))
+
+    return dict(tmp)
