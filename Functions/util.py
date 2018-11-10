8f7240efa3bdd301a979de8311b26975c3095c15
VenancioIgrejas <venancio.igrejas@poli.ufrj.br>
2018-11-10 00:35:33 -0200

    add file_exist function

--- Functions/util.py
+++ Functions/util.py
@@ -22,12 +22,21 @@ def check_mount_dict(dict_defaul,list_of_tubles):
         return dict_defaul
 
 def update_paramns(old_dict,new_dict,exception=[]):
-    for keys in old_dict:
+    for keys in new_dict:
         if not keys in exception:
-            old_dict[keys] = new_dict[keys]
+            if keys in old_dict:
+                old_dict[keys] = new_dict[keys]
     return old_dict
 
 
 def get_objects(object,name,kwargs):
+    """create a object of some instance and pass a dict
+    for the constructor
+    """
     obj = getattr(object,name)
     return obj(**kwargs)
+
+def file_exist(path_file):
+    """check if a file exists. If exists, return True
+    """
+    return os.path.isfile(path_file)
